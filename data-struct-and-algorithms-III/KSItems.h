ec03e297fe1f51ce3c51822bf57b35012dcecf13
Bartosz Pogoda <bartek.pogoda1@gmail.com>
2017-05-25 23:48:03 +0200

    work on structure, implemented item set

---
+++ data-struct-and-algorithms-III/KSItems.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <string>
+/*
+	A structure to keep input items for Knapsack problem
+	It's not safe, indexes must be validated outside
+*/
+class KSItems {
+	unsigned size, *weight, *value;
+	unsigned currentSize;
+
+public:
+	KSItems(int size) : currentSize(0), size(size), weight(new unsigned[size]), value(new unsigned[size]) {}
+
+	void addItem(unsigned weight, unsigned value) { this->weight[currentSize] = weight; this->value[currentSize++] = value; }
+
+	unsigned getSize() { return size; }
+	unsigned getWeight(int i) { return weight[i]; }
+	unsigned getValue(int i) { return value[i]; }
+
+	std::string toString();
+};
--- data-struct-and-algorithms-III/Source.cpp
+++ data-struct-and-algorithms-III/Source.cpp
@@ -1,5 +1,14 @@
+#include "KSItems.h"
 #include <iostream>
 
+
 int main() {
-	std::cout << "Project in1t";
+	KSItems ksItems = KSItems(2);
+	ksItems.addItem(1, 3);
+	ksItems.addItem(2, 4);
+
+	std::cout << ksItems.toString();
+
+	int x;
+	std::cin >> x;
 }
