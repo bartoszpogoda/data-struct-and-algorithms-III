3410c825da528fde69863a5d68d3063c85e6b619
Bartosz Pogoda <bartek.pogoda1@gmail.com>
2017-05-27 00:24:43 +0200

    implemented dynamic programming based solution for knapsack problem

---
+++ data-struct-and-algorithms-III/KSDynamic.cpp
@@ -0,0 +1,79 @@
+#include "KSDynamic.h"
+#include <sstream>
+/*
+	Result table idea (for max capacity 4)
+	
+	weight:		| 0 | 1 | 2 | 3 | 4 | 
+	val/weight	---------------------
+				| 0 | 0 | 0 | 0 | 0 | <- guardian row to avoid index == -1 check
+	5  /   4    | 0 |   |   |   |   |
+	7  /   5    | 0 |   |   |   |   |
+	1  /   1    | 0 |   |   |   | _ | <- sollution (best value)
+*/
+void KSDynamic::execute(KSItems * items, unsigned capacity) {
+	delete result;
+	result = new KSKnapsack();
+
+	// create result table
+	unsigned **resultTable = new unsigned*[capacity+1];
+	for (size_t i = 0; i <= capacity; i++) {
+		resultTable[i] = new unsigned[items->getSize() + 1];	// +1 for guardian row at the top of the table
+	}
+
+	// if the final weight was 0 - no items could be taken (weights are > 0)
+	for (size_t i = 0; i <= items->getSize(); i++) {
+		resultTable[0][i] = 0;
+	}
+
+	// guardian row
+	for (size_t i = 1; i <= capacity; i++) {
+		resultTable[i][0] = 0;
+	}
+
+	// calculations for each item 
+	for (size_t item = 0; item < items->getSize(); item++) {
+		// calculations for each (assumed) final weight
+		for (size_t maxWeight = 1; maxWeight <= capacity; maxWeight++) {
+
+			if (items->getWeight(item) > maxWeight) {
+				// item cannot be included case (item+1 index because of guardian row)
+				resultTable[maxWeight][item + 1] = resultTable[maxWeight][item];
+			} else {
+				// item can be included case - decision to make
+				if (items->getValue(item) + resultTable[maxWeight - items->getWeight(item)][item] > resultTable[maxWeight][item]) {
+					// should be included case
+					resultTable[maxWeight][item + 1] = items->getValue(item) + resultTable[maxWeight - items->getWeight(item)][item];
+				} else {
+					resultTable[maxWeight][item + 1] = resultTable[maxWeight][item];
+				}
+			}
+		}
+	}
+
+	// extract selected items
+	int maxWeight = capacity, item = items->getSize();
+	while (item > 0 && maxWeight >= 0) {
+		// if optimal value wasn't taken from upper cell - the item was included
+		if (resultTable[maxWeight][item] != resultTable[maxWeight][item - 1]) {
+			result->addItem(item - 1, items->getWeight(item - 1), items->getValue(item - 1));
+			maxWeight -= items->getWeight(item - 1);
+		} 
+
+		item -= 1;
+	}
+
+	// dealocate result table
+	for (size_t i = 0; i <= capacity; i++) {
+		delete[] resultTable[i];
+	}
+	delete[] resultTable;
+}
+
+std::string KSDynamic::toString() {
+	std::stringstream out;
+
+	out << "Solution" << std::endl << "Dynamic Programming Based" << std::endl << result->toString();
+
+	return out.str();
+}
+
--- data-struct-and-algorithms-III/Source.cpp
+++ data-struct-and-algorithms-III/Source.cpp
@@ -1,4 +1,5 @@
 #include "KSFullSearch.h"
+#include "KSDynamic.h"
 #include "KSItems.h"
 
 #include <iostream>
@@ -7,16 +8,25 @@ int main() {
 	{
 		KSFullSearch algorithm;
 
-		unsigned capacity = 50;
-		KSItems* items = new KSItems(3);
+		unsigned capacity = 60;
+		KSItems* items = new KSItems(7);
 		items->addItem(10, 60);
-		items->addItem(20, 100);
-		items->addItem(30, 120);
+		items->addItem(15, 22);
+		items->addItem(14, 22);
+		items->addItem(17, 32);
+		items->addItem(13, 12);
+		items->addItem(14, 23);
+		items->addItem(30, 44);
+
 		std::cout << "Capacity: " << capacity << std::endl;
 		std::cout << items->toString() << std::endl;
 
 		algorithm.execute(items, capacity);
 		std::cout << algorithm.toString();
+
+		KSDynamic algorithmDynamic;
+		algorithmDynamic.execute(items, capacity);
+		std::cout << algorithmDynamic.toString();
 
 		delete items;
 	}
